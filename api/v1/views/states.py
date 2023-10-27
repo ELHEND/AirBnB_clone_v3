8f872ae4b9354e0ff9fa250195da51ca0948b299
Amna2011 <amnataha2011@gmail.com>
2023-10-27 11:24:02 -0700

    mmm

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,3 +13,44 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     """Retrieves all state objects"""
+    all_states = storage.all('State')
+    all_states = [obj.to_dict() for obj in all_states.values()]
+    return jsonify(all_states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves state objects by id"""
+    state = storage.get(State, state_id)
+    if state is None:
+    abort(404, 'Not found')
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+        strict_slashes=False)
+def delete_state(state_id):
+    """Delete state by id"""
+    try:
+        state = storage.get(State, state_id)
+        state.delete()
+        storage.save()
+    except Exception:
+        abort(404, 'Not found')
+    return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Post new state object"""
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+    if 'name' not in data:
+        abort(400, "Missing name")
+
+
+    new_state = State(**data)
+    storage.new(new_state)
+    storage.save()
+    return make_response(new_state.to_dict(), 201)
