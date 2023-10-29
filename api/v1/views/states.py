6871bcd62c2cad5eb8e9bc99f3c805d9781ce6d4
Amna2011 <amnataha2011@gmail.com>
2023-10-29 10:37:19 -0700

    d

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,6 +9,7 @@ from flask import abort, request, make_response, jsonify
 from api.v1.views import app_views
 
 from models import storage
+
 from models.base_model import BaseModel
 from models.state import State
 
