e0143bd6faba28e97926230c1afbc95ecb532269
Amna2011 <amnataha2011@gmail.com>
2023-10-30 02:45:51 -0700

    h

--- models/review.py
+++ models/review.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
-""" holds class Review"""
+""" holds the class Review"""
+
 import models
 from models.base_model import BaseModel, Base
+
 from os import getenv
 import sqlalchemy
+
 from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """Representation of Review """
+    """ the Representation of Review """
     if models.storage_t == 'db':
         __tablename__ = 'reviews'
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
