3f9a242bf184b73834a50d6700d7bc5ccb1beb3b
Amna2011 <amnataha2011@gmail.com>
2023-10-30 00:44:03 -0700

    t

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from models.base_model import BaseModel
 
 from flask import jsonify, abort, request, make_response
+
 from models.city import City
 from models.user import User
 from models.place import Place
